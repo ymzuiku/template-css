7fcdf7f3d43829b6fe20e94fffffb00e90b5f5bc
pillar <ymblender@gamil.com>
2020-12-22 00:25:24 +0800

    feat: update types

--- lib/index.ts
+++ lib/index.ts
@@ -38,7 +38,45 @@ const colRow = {
   row: "row",
 } as any;
 
-css.flex = (str: string) => {
+css.flex = (
+  str:
+    | "col-start-start"
+    | "col-start-center"
+    | "col-start-end"
+    | "col-center-start"
+    | "col-center-center"
+    | "col-center-end"
+    | "col-end-start"
+    | "col-end-center"
+    | "col-end-end"
+    | "col-between-start"
+    | "col-between-center"
+    | "col-between-end"
+    | "col-around-start"
+    | "col-around-center"
+    | "col-around-end"
+    | "col-stretch-start"
+    | "col-stretch-center"
+    | "col-stretch-end"
+    | "row-start-start"
+    | "row-start-center"
+    | "row-start-end"
+    | "row-center-start"
+    | "row-center-center"
+    | "row-center-end"
+    | "row-end-start"
+    | "row-end-center"
+    | "row-end-end"
+    | "row-between-start"
+    | "row-between-center"
+    | "row-between-end"
+    | "row-around-start"
+    | "row-around-center"
+    | "row-around-end"
+    | "row-stretch-start"
+    | "row-stretch-center"
+    | "row-stretch-end"
+) => {
   const [col, justify, align] = str.split("-");
   return `
   display:flex;
