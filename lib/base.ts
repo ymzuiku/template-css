0aaa00143c9f6adf290ce873e2941e3cd5c3159e
pillar <ymblender@gamil.com>
2021-02-25 15:15:47 +0800

    feat: use default

--- lib/base.ts
+++ lib/base.ts
@@ -1,20 +1,40 @@
-# template-css
+let colors = "";
+let colorsList = [
+  "primary",
+  "gray",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "teal",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+  "light",
+  "dark",
+];
+colorsList.forEach(function (v) {
+  for (let i = 1; i <= 9; i++) {
+    colors += `--${v}-${i}00-0:rgba(let(--${v}-${i}00-base), 0); `;
+    for (let a = 1; a <= 9; a += 1) {
+      colors += `--${v}-${i}00-${a}:rgba(let(--${v}-${i}00-base), 0.${a}); `;
+    }
+    colors += `--${v}-${i}00:rgb(let(--${v}-${i}00-base)); `;
+  }
+});
+["white", "black"].forEach(function (v) {
+  colors += `--${v}-0:rgba(let(--${v}-base), 0); `;
+  for (let a = 1; a <= 9; a += 1) {
+    colors += `--${v}-${a}:rgba(let(--${v}-base), 0.${a}); `;
+  }
+  colors += `--${v}:rgb(let(--${v}-base)); `;
+});
 
-## Use
-
-```js
-import css from 'template-css';
-
-css`
-.page {
-  background: var(--primary-500);
-}
-`
-```
-## Base Css Values 
-
-```css
-:root {
+let cssValues = `
+  :root{-moz-tab-size:4;-o-tab-size:4;tab-size:4}:focus{outline:0}html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0;padding:0;font-size:16px}body{font-family:system-ui,-apple-system,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"}hr{height:0;color:inherit}abbr[title]{-webkit-text-decoration:underline dotted;text-decoration:underline dotted}b,strong{font-weight:bolder}code,kbd,pre,samp{font-family:ui-monospace,SFMono-Regular,Consolas,"Liberation Mono",Menlo,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}table{text-indent:0;border-color:inherit}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}button,select{text-transform:none}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}::-moz-focus-inner{border-style:none;padding:0}:-moz-ui-invalid{box-shadow:none}legend{padding:0}progress{vertical-align:baseline}::-webkit-inner-spin-button,::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield}::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}blockquote,dd,dl,figure,h1,h2,h3,h4,h5,h6,hr,p,pre{margin:0}button{background-color:transparent;background-image:none}fieldset{margin:0;padding:0}ol,ul{list-style:none;margin:0;padding:0}html{font-family:ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";line-height:1.5}body{font-family:inherit;line-height:inherit}*,::after,::before{box-sizing:border-box;border-width:0;border-style:solid;border-color:let(--gray-200)}hr{border-top-width:1px}img{border-style:solid}textarea{resize:vertical}input::-moz-placeholder,textarea::-moz-placeholder{color:let(--gray-400)}input:-ms-input-placeholder,textarea:-ms-input-placeholder{color:let(--gray-400)}input::placeholder,textarea::placeholder{color:let(--gray-400)}[role=button],button{cursor:pointer}table{border-collapse:collapse}h1,h2,h3,h4,h5,h6{font-size:inherit;font-weight:inherit}a{color:inherit;text-decoration:inherit}button,input,optgroup,select,textarea{padding:0;line-height:inherit;color:inherit}code,kbd,pre,samp{font-family:ui-monospace,SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace}audio,canvas,embed,iframe,img,object,svg,video{display:block;vertical-align:middle}img,video{max-width:100%;height:auto}
+  
+  :root {
   --sans: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
     "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif,
     "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
@@ -145,5 +165,10 @@ css`
   --dark-700-base: 108,108,108;
   --dark-800-base: 122,122,122;
   --dark-900-base: 133,133,133;
-}
-```
+  ${colors}
+  }
+  `;
+let ele = document.createElement("style");
+ele.textContent = cssValues;
+ele.id = "css-value-colors";
+document.head.append(ele);
--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,9 @@
+import "./base";
 import templateFn from "template-fn";
-import { cssValues } from "./cssValues";
-import { normal } from "./normal";
 
 const cache = {} as any;
 
-function css(...args: any) {
+function css(...args: any[]) {
   const txt = templateFn(...args);
   if (cache[txt]) {
     return;
@@ -14,116 +13,5 @@ function css(...args: any) {
   ele.innerHTML = txt;
   document.head.append(ele);
 }
-
-css.widthAndHeight = (str: string) => `
-width:${str};
-min-width:${str};
-max-width:${str};
-height:${str};
-min-height:${str};
-max-height:${str};
-`;
-
-const fm = {
-  center: "center",
-  start: "flex-start",
-  end: "flex-end",
-  between: "flex-between",
-  around: "flex-around",
-  auto: "auto",
-  baseline: "baseline",
-  stretch: "stretch",
-} as any;
-
-const colRow = {
-  col: "column",
-  row: "row",
-} as any;
-
-css.flex = (
-  str:
-    | "col-start-start"
-    | "col-start-center"
-    | "col-start-end"
-    | "col-center-start"
-    | "col-center-center"
-    | "col-center-end"
-    | "col-end-start"
-    | "col-end-center"
-    | "col-end-end"
-    | "col-between-start"
-    | "col-between-center"
-    | "col-between-end"
-    | "col-around-start"
-    | "col-around-center"
-    | "col-around-end"
-    | "col-stretch-start"
-    | "col-stretch-center"
-    | "col-stretch-end"
-    | "row-start-start"
-    | "row-start-center"
-    | "row-start-end"
-    | "row-center-start"
-    | "row-center-center"
-    | "row-center-end"
-    | "row-end-start"
-    | "row-end-center"
-    | "row-end-end"
-    | "row-between-start"
-    | "row-between-center"
-    | "row-between-end"
-    | "row-around-start"
-    | "row-around-center"
-    | "row-around-end"
-    | "row-stretch-start"
-    | "row-stretch-center"
-    | "row-stretch-end"
-) => {
-  const [col, justify, align] = str.split("-");
-  return `
-  display:flex;
-  flex-direction:${colRow[col]};
-  justify-content: ${fm[justify]};
-  align-items:${fm[align]};`;
-};
-
-css.clear = () => `
-margin: 0;
-border-width: 0;
-border-style: solid;
-border-color: currentColor;
--webkit-tap-highlight-color: transparent;
-outline:0;
-text-decoration:none;
-list-style: none;
-`;
-
-css.imageFitCover = () => `
-object-fit: cover;
-object-position: 50% 50%;
-`;
-
-css.wordBreak = (line = 1) => {
-  if (line === 1) {
-    return `
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    word-break: break-all;
-    `;
-  }
-  return `
-  text-overflow: -o-ellipsis-lastline;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  display: -webkit-box;
-  -webkit-line-clamp: ${line};
-  line-clamp: ${line};
-  -webkit-box-orient: vertical;
-  `;
-};
-
-css.cssValues = () => cssValues;
-css.normal = () => normal;
 
 export default css;
